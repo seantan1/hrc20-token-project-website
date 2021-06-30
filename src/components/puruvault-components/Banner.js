5b5d7e40d2285917519c48beaff588a27dbccd98
sean890 <sean1360851@gmail.com>
2021-06-30 14:13:10 +1000

    moved deposit stats to vault banner

--- src/components/puruvault-components/Banner.js
+++ src/components/puruvault-components/Banner.js
@@ -9,12 +9,17 @@ const Banner = () => {
 
     return (
         <div className="vault-banner">
-           <div className="vault-banner-image-container">
+            <div className="vault-banner-image-container">
                 <img className="vault-banner-image" src={PuruImage5}></img>
-           </div>
-           <div className="vault-banner-text">
+            </div>
+            <div className="vault-banner-text">
                 <p><span className="vault-banner-text-highlight">$PURU</span> Vault</p>
-           </div>
+            </div>
+            <div className="vault-banner-stats-container">
+                <p>Your rating: <span className="vault-banner-stats-pink">1234</span></p>
+                <p>Share in rewards: <span className="vault-banner-stats-pink">0.1%</span></p>
+                <button className="new-deposit-button">+ Deposit</button>
+            </div>
         </div>
 
     );
--- src/components/puruvault-components/Deposits.js
+++ src/components/puruvault-components/Deposits.js
@@ -5,20 +5,6 @@ const Deposits = () => {
 
     return (
         <div className="deposits">
-            <div className="deposit-stats-container">
-                <div className="deposit-stats-container-inner">
-                    <div className="deposit-stats-container-inner-text">
-                        <h1>Your rating:</h1>
-                        <p>1234</p>
-                    </div>
-                    <div className="deposit-stats-container-inner-text">
-                        <h1>Share in rewards:</h1>
-                        <p>0.1%</p>
-                    </div>
-                </div>
-                <button className="new-deposit-button">+ Deposit</button>
-            </div>
-
             <div className="deposit-box-container">
                 <div className="deposit-box">
                     <p className="deposit-id-text">ID: 344729</p>
