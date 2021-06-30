0d50899c9015c6166a6f037127acd41d87b48afd
sean890 <sean1360851@gmail.com>
2021-06-30 12:16:28 +1000

    implemented vault deposit component template

--- src/components/puruvault-components/Deposits.js
+++ src/components/puruvault-components/Deposits.js
@@ -5,10 +5,63 @@ const Deposits = () => {
 
     return (
         <div className="deposits">
-            <h1>Your Deposits</h1>
-            <div className="deposit-list-container">
-                
+            <div className="deposit-stats-container">
+                <div className="deposit-stats-container-inner">
+                    <div className="deposit-stats-container-inner-text">
+                        <h1>Your rating:</h1>
+                        <p>1234</p>
+                    </div>
+                    <div className="deposit-stats-container-inner-text">
+                        <h1>Share in rewards:</h1>
+                        <p>0.1%</p>
+                    </div>
+                </div>
+                <button className="new-deposit-button">+ Deposit</button>
             </div>
+
+            <div className="deposit-box-container">
+                <div className="deposit-box">
+                    <p className="deposit-id-text">ID: 344729</p>
+                    <h2>150 <span className="puru-text">$PURUs</span></h2>
+                    <h3>Remaining Time:</h3>
+                    <p>29 days 10 hours 54 minutes</p>
+                    <p className="deposit-ready-to-claim-text">Ready To Claim!</p>
+                    <h3>Rating & Credits earned:</h3>
+                    <p>1500</p>
+                    <button className="deposit-claim-button">Claim</button>
+                </div>
+                <div className="deposit-box">
+                    <p className="deposit-id-text">ID: 344729</p>
+                    <h2>150 <span className="puru-text">$PURUs</span></h2>
+                    <h3>Remaining Time:</h3>
+                    <p>29 days 10 hours 54 minutes</p>
+                    <p className="deposit-ready-to-claim-text">Ready To Claim!</p>
+                    <h3>Rating & Credits earned:</h3>
+                    <p>1500</p>
+                    <button className="deposit-claim-button">Claim</button>
+                </div>
+                <div className="deposit-box">
+                    <p className="deposit-id-text">ID: 344729</p>
+                    <h2>150 <span className="puru-text">$PURUs</span></h2>
+                    <h3>Remaining Time:</h3>
+                    <p>29 days 10 hours 54 minutes</p>
+                    <p className="deposit-ready-to-claim-text">Ready To Claim!</p>
+                    <h3>Rating & Credits earned:</h3>
+                    <p>1500</p>
+                    <button className="deposit-claim-button">Claim</button>
+                </div>
+                <div className="deposit-box">
+                    <p className="deposit-id-text">ID: 344729</p>
+                    <h2>150 <span className="puru-text">$PURUs</span></h2>
+                    <h3>Remaining Time:</h3>
+                    <p>29 days 10 hours 54 minutes</p>
+                    <p className="deposit-ready-to-claim-text">Ready To Claim!</p>
+                    <h3>Rating & Credits earned:</h3>
+                    <p>1500</p>
+                    <button className="deposit-claim-button">Claim</button>
+                </div>
+            </div>
+
         </div>
 
     );
