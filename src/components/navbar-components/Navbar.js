ce13ed73c6e9a06a1b62873dd147143e9712fb9e
sean890 <sean1360851@gmail.com>
2021-07-02 12:54:48 +1000

    implemented responsive webpage

--- src/components/navbar-components/Navbar.js
+++ src/components/navbar-components/Navbar.js
@@ -13,6 +13,9 @@ const Navbar = (props) => {
     const [fontColor, setfontColor] = useState("white");
     const [hoverBrandImage, setHoverBrandImage] = useState(false);
     const [hoverProfileImage, setHoverProfileImage] = useState(false);
+    // hide navbar if scrolling past the banner
+    const [hideNavBar, setHideNavBar] = useState(false);
+    const [navBarOpacity, setNavBarOpacity] = useState(1);
 
     // scroll event listener
     window.onscroll = function () {
@@ -24,56 +27,70 @@ const Navbar = (props) => {
             setbackgroundColor((prevState) => prevState = "rgba(255, 255, 255, 0.7)")
             setfontColor((prevState) => prevState = "black")
         }
+
+        // slowly hideing navbar
+        if (window.pageYOffset > 500) {
+            setHideNavBar(true);
+        }
+        else if (window.pageYOffset > 350) {
+            setNavBarOpacity((500-window.pageYOffset)/150);
+        }
+        else {
+            setHideNavBar(false);
+            setNavBarOpacity(1);
+        }
     };
     // end of hook for nav bar css change on scroll
 
     return (
         <div>
-            <div className="navbar" style={{ backgroundColor: backgroundColor }}>
-                <div className="navbar-inner">
-                    <div className="brand-container" onMouseEnter={() => setHoverBrandImage(true)} onMouseLeave={() => setHoverBrandImage(false)}>
-                        {hoverBrandImage ?
-                            <div>
-                                <a href="/"><img className="brand-image" src={PuruBrandPink} alt="puru"></img></a>
-                            </div>
-                            :
-                            <div>
-                                {fontColor === "white" ? <a href="/"><img className="brand-image" src={PuruBrandWhite} alt="puru"></img></a> : <a href="/"><img className="brand-image" src={PuruBrandBlack} alt="puru"></img></a>}
-                            </div>
-                        }
+            {!hideNavBar &&
+                <div className="navbar" style={{ backgroundColor: backgroundColor, opacity: navBarOpacity }}>
+                    <div className="navbar-inner">
+                        <div className="brand-container" onMouseEnter={() => setHoverBrandImage(true)} onMouseLeave={() => setHoverBrandImage(false)}>
+                            {hoverBrandImage ?
+                                <div>
+                                    <a href="/"><img className="brand-image" src={PuruBrandPink} alt="puru"></img></a>
+                                </div>
+                                :
+                                <div>
+                                    {fontColor === "white" ? <a href="/"><img className="brand-image" src={PuruBrandWhite} alt="puru"></img></a> : <a href="/"><img className="brand-image" src={PuruBrandBlack} alt="puru"></img></a>}
+                                </div>
+                            }
 
-                    </div>
-
-                    <div className="nav navbar-links">
-                        <a style={{ color: fontColor }} href="/">Home</a>
-                        <a style={{ color: fontColor }} href="/vault">Vault</a>
-                        <a style={{ color: fontColor }} href="/credits-store">Credits Store</a>
-                        <a style={{ color: fontColor }} href="/bounty">Claim Bounty</a>
-                        <a style={{ color: fontColor }} href="/info">Info</a>
-                    </div>
-                    <div className="profile-container" onClick={props.toggleProfileWindow} onMouseEnter={() => setHoverProfileImage(true)} onMouseLeave={() => setHoverProfileImage(false)}>
-                        {hoverProfileImage ?
-                            <div>
-                                <img className="profile-image" src={PuruBrandPink} alt="puru"></img>
-                            </div>
-                            :
-                            <div>
-                                {fontColor === "white" ? <img className="profile-image" src={PuruBrandWhite} alt="puru"></img> : <img className="profile-image" src={PuruBrandBlack} alt="puru"></img>}
-                            </div>
-                        }
-                    </div>
-                    {props.transactionPending &&
-                        <div className="tx-pending-container">
-                            <img src={loadingGIF} id="loading-gif" alt="loading"></img>
-                            <p style={{ color: fontColor }}>Transaction pending</p>
                         </div>
-                    }
-                    <div className="connect-wallet-button-container">
-                        {!props.authorised && <button className="connect-wallet-button" onClick={props.toggleWalletWindow}>Connect Wallet</button>}
-                        {props.authorised && <button className="connect-wallet-button" onClick={props.toggleWalletWindow}>{props.account.substring(0, 11)}...</button>}
+
+                        <div className="nav navbar-links">
+                            <a style={{ color: fontColor }} href="/">Home</a>
+                            <a style={{ color: fontColor }} href="/vault">Vault</a>
+                            <a style={{ color: fontColor }} href="/credits-store">Credits Store</a>
+                            <a style={{ color: fontColor }} href="/bounty">Claim Bounty</a>
+                            <a style={{ color: fontColor }} href="/info">Info</a>
+                        </div>
+                        <div className="profile-container" onClick={props.toggleProfileWindow} onMouseEnter={() => setHoverProfileImage(true)} onMouseLeave={() => setHoverProfileImage(false)}>
+                            {hoverProfileImage ?
+                                <div>
+                                    <img className="profile-image" src={PuruBrandPink} alt="puru"></img>
+                                </div>
+                                :
+                                <div>
+                                    {fontColor === "white" ? <img className="profile-image" src={PuruBrandWhite} alt="puru"></img> : <img className="profile-image" src={PuruBrandBlack} alt="puru"></img>}
+                                </div>
+                            }
+                        </div>
+                        {props.transactionPending &&
+                            <div className="tx-pending-container">
+                                <img src={loadingGIF} id="loading-gif" alt="loading"></img>
+                                <p style={{ color: fontColor }}>Transaction pending</p>
+                            </div>
+                        }
+                        <div className="connect-wallet-button-container">
+                            {!props.authorised && <button className="connect-wallet-button" onClick={props.toggleWalletWindow}>Connect Wallet</button>}
+                            {props.authorised && <button className="connect-wallet-button" onClick={props.toggleWalletWindow}>{props.account.substring(0, 11)}...</button>}
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
 
     );
