f14e78be3fe70de79949c1110804bfde8091bc12
sean890 <sean1360851@gmail.com>
2021-07-02 14:58:40 +1000

    update tokenomics info

--- src/components/info-components/Info.js
+++ src/components/info-components/Info.js
@@ -19,7 +19,7 @@ const Info = () => {
             <h1>Tokenomics</h1>
             <p>This section will explain all there is to know on how the entire system work.</p>
             <h3>Slash Fee</h3>
-            <p>A 35% slash fee will be imposed on ALL outbound transfers made by users, this includes:</p>
+            <p>A 20% slash fee will be imposed on ALL outbound transfers made by users, this includes:</p>
             <p><strong>1.</strong> Selling at a DEX/Liquidity pool, as you "transfer out" your $PURUs to the liquidity pool, the liquidity pool is also smart
             enough to pay the correct amount after the slash fee is taken.</p>
             <p><strong>2.</strong> Transferring to another wallet or unauthorised contract, so please be careful to keep your $PURU on just one wallet!</p>
@@ -32,8 +32,28 @@ const Info = () => {
             not get taxed for creating or claiming a deposit.</p>
             <p><strong>3.</strong> Developers fund vault is also exempt from the outbound transfer slash fee for reasons such as adding liquidity, airdrop events and paying bounties. 
             All related wallet and contract addresses will be listed below and transparent to all users.</p>
-            <p>The slash fee is in place to give holders a peace of mind that not only panic sellers and ruggers have a less 35% impact on 
+            <p>The slash fee is in place to give holders a peace of mind that not only panic sellers and ruggers have a less 20% impact on 
             the market, but also having that slash fee rewarded back to them!</p>
+
+            <h3>Locked Vault</h3>
+            <p>$PURU can be staked into the locked vault between 30 to 100 days (only whole days are acceptable such as 31 or 84 days, not 34.5 days).
+            Users who deposit their $PURU will receive an increase in rating which is calculated as such:</p>
+            <p><strong>Rating</strong> = ( $PURU deposited ) x ( Duration in days )^2 / 100</p>
+            <p>Example: Depositing 10 $PURU for 30 days will give you 90 rating (stackable with every deposit made)</p>
+            <p>In plain terms, users who choose a longer staking period gains a much higher rating compared to shorter staking periods, while a 
+            100-day cap is also added so that short-term stakers can get a fair share of the rewards pool as well.</p>
+            <br></br>
+            <p>Users can have unlimited deposits at a time and stack as much rating as they like, as more rating would mean a bigger share 
+            in slash fees collected in the reward pool. Your share in the pool is calculated as such:</p>
+            <p>Your share = ( Your Total rating ) / ( Total rating of all users ) x 100%</p>
+            <p>Example: If your rating is 100 and the total rating of all users is 1000, you own 10% in the reward pool share (with some deductions explained below).</p>
+            <br></br>
+            <p>As to not impose a force lock on users, there is a forfeit option for any locked deposit, where only 50% of the tokens will 
+            be returned to the user while the rest goes into the developer's vualt. This is so that in the case any user accidentally forfeited their 
+            deposits, devs can return the tokens to the user upon consideration. Any remaining tokens sent to the dev's vault this way will be 
+            used for event rewards or burned.</p>
+            <p>Another thing to note about it that your rating will be reduced upon claiming your $PURU from a deposit, the reduced amount 
+            is the amount you gained from that same deposit.</p>
 
             <h3>Exemption quota to slash fee</h3>
             <p>Users who stake their $PURU in the locked vault will receive an exemption quota to slash fee the moment they claim their $PURU. 
@@ -43,24 +63,6 @@ const Info = () => {
             <p>The exemption quota is flexible and permanent, which can be spend on multiple transfers. On a transfer exceeding the exemption 
             quota, the remaining exemption quota will waiver the correct portion of tokens from the slash fee while taxing the other portion. 
             Thus, there is nothing to worry about, all is fair and square!</p>
-            
-            <h3>Locked Vault</h3>
-            <p>$PURU can be staked into the locked vault between 30 to 180 days (only whole days are acceptable such as 31 or 84 days, not 34.5 days).
-            Users who deposit their $PURU will receive an increase in rating which is calculated as such:</p>
-            <p>Rating = $PURU deposited x Duration (days)</p>
-            <p>Example: Depositing 10 $PURU for 30 days will give you 300 rating (stackable with every deposit made)</p>
-            <br></br>
-            <p>Users can have up to 100 deposits at a time and stack as much rating as they like, as more rating would mean a bigger share 
-            in slash fees collected in the reward pool. Your share in the pool is calculated as such:</p>
-            <p>Your share = (Your Total rating) / (Total rating of all users) x 100%</p>
-            <p>Example: If your rating is 100 and the total rating of all users is 1000, you own 10% in the reward pool share (with some deductions explained below).</p>
-            <br></br>
-            <p>As to not impose a force lock on users, there is a forfeit option for any locked deposit, where only 50% of the tokens will 
-            be returned to the user while the rest goes into the developer's vualt. This is so that in the case any user accidentally forfeited their 
-            deposits, devs can return the tokens to the user upon consideration. Any remaining tokens sent to the dev's vault this way will be 
-            used for event rewards or burned.</p>
-            <p>Another thing to note about it that your rating will be reduced upon claiming your $PURU from a deposit, the reduced amount 
-            is the amount you gained from that same deposit.</p>
 
             <h3>Bounty Reward and Dev cut</h3>
             <p>0.1% of the reward pool is paid out as a bounty reward for the user who paid the gas fee to execute the reward distribution. 
