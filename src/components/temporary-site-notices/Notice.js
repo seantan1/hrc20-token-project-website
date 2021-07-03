a4a6298b15a2f37da26f553cce205836c06055ba
sean890 <sean1360851@gmail.com>
2021-07-03 20:31:20 +1000

    minor edits to css and text

--- src/components/temporary-site-notices/Notice.js
+++ src/components/temporary-site-notices/Notice.js
@@ -3,7 +3,7 @@ const Notice = () => {
     return (
         <div className="temp-notice">
             <div className="temp-notice-container">
-                <h1><span className="red-text">WARNING: PROJECT IS ON BETA TESTING</span></h1>
+                <h1><span className="red-text">WARNING: PROJECT IS ON BETA TESTING PHASE</span></h1>
                 <p>Current token and contracts are not official and will be replaced on official launch. Please do not buy any $PURU on any dexes 
                 claiming to be the official $PURU. Devs are not responsible for any losses occured.</p>
             </div>
