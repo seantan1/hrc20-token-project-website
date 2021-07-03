492636c1d23ac347fae58513e763b4e79c6f0753
sean890 <sean1360851@gmail.com>
2021-07-03 14:36:45 +1000

    Update Footer.js

--- src/components/footer-components/Footer.js
+++ src/components/footer-components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-links-container">
-                <a href="/">Discord</a> | <a href="/">Twitter</a> | <a href="/">GitHub</a>
+                <a href="https://discord.gg/m7RsBDKhqX" target="_blank" rel="noreferrer">Discord</a> | <a href="/">Twitter</a> | <a href="/">GitHub</a>
             </div>
             
         </div>
