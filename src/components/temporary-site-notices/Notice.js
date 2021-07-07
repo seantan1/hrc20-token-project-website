74d6c702034443903d55f5793721ca09ea1e7f65
sean890 <sean1360851@gmail.com>
2021-07-07 14:44:50 +1000

    added IDO date

--- src/components/temporary-site-notices/Notice.js
+++ src/components/temporary-site-notices/Notice.js
@@ -3,9 +3,9 @@ const Notice = () => {
     return (
         <div className="temp-notice">
             <div className="temp-notice-container">
-                <h1><span className="red-text">WARNING: PROJECT IS ON BETA TESTING PHASE</span></h1>
+                <h1><span className="red-text">IDO Launch: 14/07/21 12~1pm GMT</span></h1>
                 <p>Current token and contracts are not official and will be replaced on official launch. Please do not buy any $PURU on any dexes 
-                claiming to be the official $PURU. Devs are not responsible for any losses occured.</p>
+                claiming to be the official $PURU. Devs are not responsible for any losses occured. This section will be removed on launch.</p>
             </div>
             
         </div>
