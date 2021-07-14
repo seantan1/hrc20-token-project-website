d9c94835b7289fc65567af32492a8b1485872d3d
seantan1 <seantan374@gmail.com>
2021-07-14 23:05:48 +1000

    added viperswap button link

--- src/components/home-components/Banner.js
+++ src/components/home-components/Banner.js
@@ -14,7 +14,10 @@ const Banner = () => {
            </div>
            <div className="banner-text">
                 <p>Every day is <span className="banner-text-highlight">$PURU</span> day!</p>
-                <button className="buy-now-button">Buy on Viperswap</button>
+                <button className="buy-now-button" onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href='https://viper.exchange/#/swap?inputCurrency=ONE&outputCurrency=0x1d611c8F37bc96772653B11d9410A2B71C684754';
+                    }}>Buy on Viperswap</button>
            </div>
         </div>
 
